e85590542031b2d882d06a2606b8a2b553d15b52
hanifaudah <hanifarkanaudah@gmail.com>
2023-10-01 21:25:39 +0700

    refactor: remove commented code

--- compiler_🙏.c
+++ compiler_🙏.c
@@ -7,7 +7,7 @@ char* get_file_contents(char *input_file) {
   FILE *f = fopen(input_file, "rb");
   fseek(f, 0, SEEK_END);
   long fsize = ftell(f);
-  fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
+  fseek(f, 0, SEEK_SET);
 
   char *string = malloc(fsize + 1);
   fread(string, fsize, 1, f);
@@ -24,33 +24,18 @@ char* handle_mem(int *mem, int *pntr, char *token) {
   char decrement[] = "🙏🙏🙏🙏🙏";
   char input[] = "🙏🙏🙏🙏🙏🙏";
   char output[] = "🙏🙏🙏🙏🙏🙏🙏";
-  // const int mem_length = 30000;
   char *compiled = "";
   if (strcmp(token, shift_left) == 0) {
-    // (*pntr)--;
-    // if (pntr < 0) {
-    //   *pntr = mem_length;
-    // }
     compiled = "pntr--;if(pntr < 0){pntr = mem_length - 1;}";
   } else if (strcmp(token, shift_right) == 0) {
-    // (*pntr)++;
-    // if (*pntr > mem_length) {
-    //   *pntr = 0;
-    // }
     compiled = "pntr++;if(pntr>=mem_length){pntr=0;}";
   } else if (strcmp(token, increment) == 0) {
-    // mem[*pntr]++;
     compiled = "mem[pntr]++;";
   } else if (strcmp(token, decrement) == 0) {
-    // mem[*pntr]--;
     compiled = "mem[pntr]--;";
   } else if (strcmp(token, input) == 0) {
-    // char input;
-    // scanf("%c", &input);
-    // mem[*pntr] = (int) input;
     compiled = "char input;scanf(\"%c\", &input);mem[pntr] = (int) input;";
   } else if (strcmp(token, output) == 0) {
-    // printf("%c", mem[*pntr]);
     compiled = "printf(\"%c\", mem[pntr]);";
   }
   return compiled;
@@ -99,10 +84,6 @@ int main(int argc, char *argv[]) {
   }
   fprintf(fptr, "%s", "}");
   fclose(fptr);
-
-  // for (int i = 0; i < 10; i++) {
-  //   printf("%d ", mem[i]);
-  // }
 
   char command[7 + 12 + strlen(output_file)];
   sprintf(command, "gcc -o %s %s", output_file, compiled_file);
